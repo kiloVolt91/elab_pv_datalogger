a6adadbdd91bf1d3fc91e4fabaa5cff53e7638d8
kiloVolt91 <pisciotta.enrico@gmail.com>
2023-04-27 10:46:14 +0200

    Update live plotter stringhe

--- live_plotter_string_bar.py
+++ live_plotter_string_bar.py
@@ -10,8 +10,9 @@ import sqlalchemy
 import time
 import matplotlib.pyplot as plt
 import matplotlib
-import math
 import matplotlib.ticker as ticker
+import math
+
 matplotlib.use("TkAgg")
 
 
@@ -31,23 +32,6 @@ def update_valori_grafico(df_day, now):
     for data in update_data:
         update_orario.append(str(data.hour).zfill(2)+':'+str(data.minute).zfill(2))
     return(update_potenza, update_irraggiamento, update_orario)
-
-def get_performance_ratio(update_potenza, update_orario, update_irraggiamento):
-        irr_std =1000
-        p_std = 300
-        power_performance = np.divide(update_potenza,p_std)
-        irr_performance = np.divide(update_irraggiamento,irr_std)
-        prestazione=[]
-        for i in range (0, len(update_orario)):
-            if irr_performance[i] == 0:
-                prestazione.append(0);
-            else:
-                perf_ratio = power_performance[i]/irr_performance[i]
-                if perf_ratio > 1:
-                    prestazione.append(1)
-                else:                
-                    prestazione.append(perf_ratio)
-        return(prestazione)
 
 def get_string_box_indexes (df_day):
     colonne_df = df_day.columns.tolist()
@@ -103,30 +87,31 @@ while True:
     df_day= download_df_from_db(url, now)
     liste = get_string_box_indexes (df_day)
     update_potenza, update_irraggiamento, update_orario = update_valori_grafico(df_day, now)
-    prestazione = get_performance_ratio(update_potenza, update_orario, update_irraggiamento)
     label_asse_x = update_orario  
     indici_asse_x = np.linspace(1, len(update_orario), len(update_orario))
     valore_nominale = 5210
     bar_width=0.2
     bottom = np.zeros(3)
     k=0
-    for j in range (0, len(liste)): 
-        for i in range (0, len(liste[j])):
-            data = df_day[liste[j][i]].iloc[-1]
-            delta = valore_nominale - data
-            if delta < 0:
-                delta = 0
-            nomi[k].remove()
-            ax[i,j].bar(0,data,bar_width, color = '#ff7f0e') 
-            ax[i,j].bar(0,delta,bar_width, color = '#1f77b4',bottom=data)
-            nomi[k] = ax[i,j].text(0, 5500, str(data)[0:4], color="orangered", verticalalignment="center", horizontalalignment="center", size=18)
-            ax[i,j].grid(visible=True)
-            ax[i,j].title.set_text("Stringa - "+str(liste[j][i]))
-            ax[i,j].set_ylim(ymin=0, ymax=6500)
-            ax[i,j].yaxis.set_major_locator(ticker.MaxNLocator(7))
-            ax[i,j].yaxis.set_minor_locator(ticker.MaxNLocator(7))
-            ax[i,j].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-            k=k+1
+    if df_day.empty == False:
+        for j in range (0, len(liste)): 
+            for i in range (0, len(liste[j])):
+                data = df_day[liste[j][i]].iloc[-1]
+                delta = valore_nominale - data
+                if delta < 0:
+                    delta = 0
+                nomi[k].remove()
+                ax[i,j].cla()
+                ax[i,j].bar(0,data,bar_width, color = '#ff7f0e') 
+                ax[i,j].bar(0,delta,bar_width, color = '#1f77b4',bottom=data)
+                nomi[k] = ax[i,j].text(0, 5500, str(data)[0:4], color="orangered", verticalalignment="center", horizontalalignment="center", size=18)
+                ax[i,j].grid(visible=True)
+                ax[i,j].title.set_text("Stringa - "+str(liste[j][i]))
+                ax[i,j].set_ylim(ymin=0, ymax=6500)
+                ax[i,j].yaxis.set_major_locator(ticker.MaxNLocator(7))
+                ax[i,j].yaxis.set_minor_locator(ticker.MaxNLocator(7))
+                ax[i,j].tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+                k=k+1
 
     figure.subplots_adjust(wspace=0.5, hspace=0.2)
     figure.canvas.draw()
